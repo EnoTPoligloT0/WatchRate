bb933907c364bfd220c127d48db604dca615b651
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-02-04 23:42:22 +0100

    EfCoreValueConverterAttribute

---
+++ WatchRate.Domain/Common/Attributes/EfCoreValueConverterAttribute.cs
@@ -0,0 +1,12 @@
+namespace WatchRate.Domain.Common.Attributes;
+
+
+public class EfCoreValueConverterAttribute : Attribute
+{
+    public EfCoreValueConverterAttribute(Type valueConverter)
+    {
+        ValueConverter = valueConverter;
+    }
+
+    public Type ValueConverter { get; }
+}
