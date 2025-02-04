8157f81bde20c951a259a451ebb5c3f5b100c5c7
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-02-04 23:54:47 +0100

    MaturityRating enum

---
+++ WatchRate.Domain/MovieAggregate/ValueObjects/MaturityRating.cs
@@ -0,0 +1,15 @@
+namespace WatchRate.Domain.MovieAggregate.ValueObjects;
+public enum MaturityRating
+{
+    G,
+    PG,
+    PG13,
+    R,
+    NC17,
+    TVY,
+    TVY7,
+    TVG,
+    TVPG,
+    TV14,
+    TVMA
+}
