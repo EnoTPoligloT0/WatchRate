94153ea5db3489e86f1edd6ea69c6ecfb51515cc
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-02-06 23:55:48 +0100

    UserRatingId implementation

--- WatchRate.Domain/UserAggregate/ValueObjects/UserRatingId.cs
+++ WatchRate.Domain/UserAggregate/ValueObjects/UserRatingId.cs
@@ -1,6 +1,21 @@
+using WatchRate.Domain.Common.Models;
+
 namespace WatchRate.Domain.UserAggregate.ValueObjects;
 
-public class UserRatingId
+public class UserRatingId : ValueObject, IEntityId<UserRatingId, Guid>
 {
+    public Guid Value { get; }
+    public UserRatingId(Guid value)
+    {
+        Value = value;
+    }
     
+    public static UserRatingId CreateUnique() => new (Guid.NewGuid());
+
+    public static UserRatingId Create(Guid value) => new (value);
+    
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
 }
