e2856a948b2ea605b36812ad32f5f6262db849b3
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-02-10 23:50:42 +0100

    DependencyInjection with AddPersistence method

---
+++ WatchRate.Infrastucture/DependencyInjection.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WatchRate.Infrastucture.Persistance;
+using Microsoft.Extensions.Configuration;
+
+namespace WatchRate.Infrastucture;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<WatchRateDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+        return services;
+    }
+}
