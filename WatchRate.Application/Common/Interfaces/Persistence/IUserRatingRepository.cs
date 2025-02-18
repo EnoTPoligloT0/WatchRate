fa11efbb74f26691fa3f96e4775dbb75180e4234
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-02-18 23:45:29 +0100

    UserRating repository interface

---
+++ WatchRate.Application/Common/Interfaces/Persistence/IUserRatingRepository.cs
@@ -0,0 +1,13 @@
+using WatchRate.Domain.UserAggregate.Entities;
+using WatchRate.Domain.UserAggregate.ValueObjects;
+using WatchRate.Domain.MovieAggregate.ValueObjects;
+
+namespace WatchRate.Application.Common.Interfaces.Persistence;
+
+public interface IUserRatingRepository
+{
+    Task<IEnumerable<UserRating>> GetByUserId(UserId userId);
+    Task AddRating(UserId userId, MovieId movieId, int value, string? review);
+    Task UpdateRating(UserId userId, MovieId movieId, int value, string? review);
+    Task RemoveRating(UserId userId, MovieId movieId);
+} 
