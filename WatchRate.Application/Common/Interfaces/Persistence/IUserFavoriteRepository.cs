639b12e21e15bfc8d6f9112ded259e3386f98ed7
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-02-18 23:47:07 +0100

    UserFavorite repository interface

---
+++ WatchRate.Application/Common/Interfaces/Persistence/IUserFavoriteRepository.cs
@@ -0,0 +1,12 @@
+using WatchRate.Domain.UserAggregate.Entities;
+using WatchRate.Domain.UserAggregate.ValueObjects;
+using WatchRate.Domain.MovieAggregate.ValueObjects;
+
+namespace WatchRate.Application.Common.Interfaces.Persistence;
+
+public interface IUserFavoriteRepository
+{
+    Task<IEnumerable<UserFavorite?>> GetByUserId(UserId userId);
+    Task AddToFavorites(UserId userId, MovieId movieId);
+    Task RemoveFromFavorites(UserId userId, MovieId movieId);
+} 
