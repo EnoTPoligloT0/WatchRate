393ea2ca7532658ed18b79533afcf011a316ca9c
EnoTPoligloT0 <artem.ladovshchyk@microsoft.wsei.edu.pl>
2025-02-22 23:56:15 +0100

    Add all repositories as a scope

--- WatchRate.Infrastucture/DependencyInjection.cs
+++ WatchRate.Infrastucture/DependencyInjection.cs
@@ -1,7 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using WatchRate.Application.Common.Interfaces.Persistence;
+using WatchRate.Application.Common.Interfaces.Persistence.Movie;
 using WatchRate.Infrastucture.Persistence;
+using WatchRate.Infrastucture.Persistence.Repositories;
 
 namespace WatchRate.Infrastucture;
 
@@ -13,6 +16,17 @@ public static class DependencyInjection
         
         services.AddDbContext<WatchRateDbContext>(options =>
             options.UseNpgsql(connectionString));
+
+        services.AddScoped<IMovieRepository, MovieRepository>();
+        services.AddScoped<IMovieCastRepository, MovieCastRepository>();
+        services.AddScoped<IMovieCrewRepository, MovieCrewRepository>();
+        services.AddScoped<IMovieGenreRepository, MovieGenreRepository>();
+        services.AddScoped<IStreamingPlatformRepository, StreamingPlatformRepository>();
+        services.AddScoped<IPersonRepository, PersonRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUserWatchlistRepository, UserWatchlistRepository>();
+        services.AddScoped<IUserFavoriteRepository, UserFavoriteRepository>();
+        services.AddScoped<IUserRatingRepository, UserRatingRepository>();
         
         return services;
     }
